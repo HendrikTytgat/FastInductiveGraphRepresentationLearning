5c49959865246c2ae7e48a67a0e80454ab2417b8
Hendrik <36707587+HendrikTytgat@users.noreply.github.com>
2020-09-18 16:16:58 +0200

    Add files via upload

---
+++ FastInductiveGraphRepresentationLearning/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 18 16:01:01 2020
+
+@author: hendr
+"""
+from FastInductiveGraphRepresentationLearning.FIGRL import FIGRL
+from FastInductiveGraphRepresentationLearning.FIGRL_always_newS import FIGRL_version2
+from FastInductiveGraphRepresentationLearning.FIGRL_expanding_trainS import FIGRL_version3
+
